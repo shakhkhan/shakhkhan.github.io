d4ebb3c18a7822877e2c6cd5c3accfe043efda2f
shakhkhan <38525940+shakhkhan@users.noreply.github.com>
2018-07-15 02:08:59 +0530

    Add files via upload

---
+++ js/wrap.js
@@ -0,0 +1,24 @@
+
+// Wrap every letter in a span
+$('.first-name').each(function(){
+  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.first-name .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: function(el, i) {
+      return 100*i;
+    }
+  }).add({
+    targets: '.first-name',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
